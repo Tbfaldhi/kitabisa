94ce9277919c1249e4b1e8a4855c91133f60fe66
Tubagus Faldhie Arrachman <30398386+Tbfaldhi@users.noreply.github.com>
2020-07-25 18:33:21 +0700

    Update queryanswer.sql
    add comment

--- queryanswer.sql
+++ queryanswer.sql
@@ -1,15 +1,16 @@
+/* Please select order from low price to high price with all detail */
 SELECT * FROM Order
 ORDER BY amount ASC;
 
+
+/*Display the cust_name  who purchased the prod_id A101 and A103 */
 SELECT  Customer.cust_name
 FROM Order 
 INNER JOIN Customer 
 ON Order.cust_id=Customer.id;
 WHERE Order.prod_id =’A101 ’ OR OROrder.prod_id=’A103 ’ 
 
-
-
-
+/*Display All detail in table Product purchased by Budi */
 SELECT Product.prod_name,Product.prod_amount,Product.vendor_id	 
 FROM Customer
 JOIN Order 
